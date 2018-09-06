293617a907f4b534d3f03dc4f5538747fc809086
Brett Wilson <brett.wilson1@marist.edu>
2018-09-06 15:08:37 -0400

    Solution to Curiosity rover exercise.

---
+++ Rover.py
@@ -0,0 +1,14 @@
+# CMPT 120L 113
+# Mars Rover Program
+# Author: Brett Wilson
+# Created: 6 Sep 2018
+
+def main():
+    time = (34000000/186000)/60 
+    print("It takes", time, "minutes for a photo to reach NASA from the Mars Curiosity")
+    
+    
+
+main()
+
+
